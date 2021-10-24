f273ae8a0c2c690a5560c03e76992fcb2c1caa7f
gelugu <mkraev@vtb.ru>
2021-10-24 13:33:19 +0300

    init app

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { render } from "react-dom";
+
+const App = () => {
+  return <div>Hello basic react app!</div>;
+};
+
+render(<App />, document.getElementById("app"));
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+};
