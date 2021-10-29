5feff3763755259e312505f6492b827817d1ae5f
gelugu <mkraev@vtb.ru>
2021-10-29 19:24:18 +0300

    add buttons

---
+++ src/components/button/Button.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+export const Button = () => {
+  return <Wrapper>
+    aa
+  </Wrapper>;
+};
+
+const Wrapper = styled.button`
+  border: none;
+`;
