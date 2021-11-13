c063050e0677a4932ad83bb57dbda3f6f3c05a4a
gelugu <gelugu@users.noreply.github.com>
2021-11-13 07:51:39 +0000

    deploy: b06d150a78ebcb9a45762963c474491f0e0c9c01

--- components/layout/WebLayout.d.ts
+++ components/layout/WebLayout.d.ts
@@ -1,2 +1,8 @@
-import { ViewProps } from "../view";
-export declare const WebLayout: (props: ViewProps) => JSX.Element;
+import { ReactNode } from "react";
+export declare const WebLayout: (props: WebLayoutProps) => JSX.Element;
+export interface WebLayoutProps {
+    children?: ReactNode;
+    direction?: "column" | "row";
+    positionX?: "start" | "center" | "end";
+    positionY?: "start" | "center" | "end";
+}
