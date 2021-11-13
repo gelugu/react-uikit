c4b58ebdedf3a9fd8358ae6e87dcae1ca97aa69e
gelugu <gelugu@users.noreply.github.com>
2021-11-13 13:49:19 +0000

    deploy: 7c8d1b60c780f6c284e26c9f109fbf582a57d5c2

--- components/animation/FloatAnimation.d.ts
+++ components/animation/FloatAnimation.d.ts
@@ -3,6 +3,7 @@ export declare const FloatAnimation: (props: FloatAnimationProps) => JSX.Element
 export interface FloatAnimationProps {
     children: ReactNode;
     fromPosition?: Coord2d;
+    toPosition?: Coord2d;
 }
 export interface Coord2d {
     x: number;
